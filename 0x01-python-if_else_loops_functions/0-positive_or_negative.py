bc4f6a2deeab7b068113959882f65e6f3bdbea02
kweenDev <refiloe15radebe@gmail.com>
2023-12-30 00:37:59 +0200

    Add script that generates a random number and identifies if it is positive or negative.

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import random
+
+def positive_or_negative():
+    """
+    Assigns a random signed number to the variable number and
+    prints whether it is positive, negative, or zero.
+    """
+    number = random.randint(-10, 10)
+
+    print(f"{number} ", end="")
+
+    if number > 0:
+        print("is positive")
+    elif number == 0:
+        print("is zero")
+    else:
+        print("is negative")
+
+if __name__ == "__main__":
+    positive_or_negative()
