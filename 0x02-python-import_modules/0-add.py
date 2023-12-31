c2a078ad663ff7fbe9a497a57ee22c9d989480bb
kweenDev <refiloe15radebe@gmail.com>
2023-12-31 14:26:11 +0200

    feat: Import 'add' function from add_0.py and print the result
    This commit adds a script (0-add.py) that imports the 'add' function from add_0.py and prints the result of the addition of 1 and 2.

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+# Import the add function from add_0.py
 from add_0 import add
 
-
+# Assign the values 1 and 2 to a and b
 a = 1
 b = 2
 
-result = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, result))
+# Print the result of the addition using string formatting
+print(f"{a} + {b} = {add(a, b)}")
