ce8ded454c9af345adade20861884fef19acbb89
kweenDev <refiloe15radebe@gmail.com>
2023-12-31 13:43:28 +0200

    feat: Implement 2-args.py for dynamic script with command-line arguments
    This commit adds a script (2-args.py) that prints the number and list of command-line arguments, showcasing the use of sys.argv.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+num_args = len(argv)
+
+
+if num_args == 1:
+    print("0 arguments.")
+elif num_args == 2:
+    print("1 argument:")
+    print("1: {}".format(argv[1]))
+else:
+    print("{} arguments:".format(num_args - 1))
+    for i in range(1, num_args):
+        print("{}: {}".format(i, argv[i]))
