8cd930949d0ac27144ac518584f40417d0caede3
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 01:11:09 +0200

    Develop Python script 11-delete_at.py for deleting an item at a specific position in a list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def delete_at(my_list=[], idx=0):
     """
     Deletes the item at a specific position in a list.
@@ -11,46 +12,10 @@ def delete_at(my_list=[], idx=0):
     Returns:
     - A new list with the item at the specified position removed.
     """
-    if idx < 0 or idx >= len(my_list):
-        return my_list.copy()  # Ensure the original list is not modified
-    return my_list[:idx] + my_list[idx+1:]
+    if 0 <= idx < len(my_list):
+        del(my_list[idx])
+    return (my_list)
+
 
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
-
-    # Test case 1
-    idx = 3
-    new_list = delete_at(my_list, idx)
-    print(new_list)
-    print(my_list)
-
-    # Test case 2
-    idx = 0
-    new_list = delete_at(my_list, idx)
-    print(new_list)
-    print(my_list)
-
-    # Test case 3
-    idx = 4
-    new_list = delete_at(my_list, idx)
-    print(new_list)
-    print(my_list)
-
-    # Test case 4
-    idx = 5
-    new_list = delete_at(my_list, idx)
-    print(new_list)
-    print(my_list)
-
-    # Test case 5
-    idx = -1
-    new_list = delete_at(my_list, idx)
-    print(new_list)
-    print(my_list)
-
-    # Test case 6
-    my_list = []
-    idx = 0
-    new_list = delete_at(my_list, idx)
-    print(new_list)
-    print(my_list)
