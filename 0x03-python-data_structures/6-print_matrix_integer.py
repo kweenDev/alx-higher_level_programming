220ab45ac4a4d22f6e0b99855a78770fed925599
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 00:29:59 +0200

    Implement Python script 6-print_matrix_integer.py for printing a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    """
+    Print a matrix of integers.
+
+    Args:
+    - matrix: 2D list of integers to be printed.
+    """
+    for row in matrix:
+        print(' '.join(map(str, row)))
+
+
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    print_matrix_integer(matrix)
+    print("--")
+    print_matrix_integer()
