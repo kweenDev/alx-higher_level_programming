1766f0d7d76dddcc1512e7e1ffdde369df157e23
kweenDev <refiloe15radebe@gmail.com>
2024-01-02 15:46:35 +0200

    feat: implement function to print a list of integers
    This commit adds a Python script with a function that prints all integers of a given list, meeting the requirements of Task 0.

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
+
 def print_list_integer(my_list=[]):
     """
     Prints all integers of a list.
@@ -12,4 +15,3 @@ def print_list_integer(my_list=[]):
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
     print_list_integer(my_list)
-
