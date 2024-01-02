2f73d996a03043aee3c3d5fe6ebc5586b325f8cf
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 01:53:19 +0200

    Code Python script 3-print_reversed_list_integer.py for printing integers in reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,12 +2,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    """
-    Print all integers of a list in reverse order.
 
-    Args:
-    - my_list: List of integers to be printed in reverse.
-    """
-     if my_list:
-         for i in reversed(my_list):
-             print('{:d}'.format(i))
+    if my_list:
+        for i in reversed(my_list):
+            print('{:d}'.format(i))
