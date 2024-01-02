d03b72675ea5e0fd087f9580919376b2b9e83eb0
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 00:36:16 +0200

    Develop Python script 7-add_tuple.py for adding two tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    """
+    Adds two tuples element-wise.
+
+    Args:
+    - tuple_a: First tuple.
+    - tuple_b: Second tuple.
+
+    Returns:
+    - A new tuple containing the element-wise sum.
+    """
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    return a[0] + b[0], a[1] + b[1]
+
+
+if __name__ == "__main__":
+    tuple_a = (1, 89)
+    tuple_b = (88, 11)
+    new_tuple = add_tuple(tuple_a, tuple_b)
+    print(new_tuple)
+
+    print(add_tuple(tuple_a, (1,)))
+    print(add_tuple(tuple_a, ()))
