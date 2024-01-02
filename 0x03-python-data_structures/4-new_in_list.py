e7ce383c74acbe5cdf58ee6e4b71a551a4f11e12
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 00:06:47 +0200

    Write Python script 4-new_in_list.py for replacing an element in a list without modifying the original

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,8 @@
 
 def new_in_list(my_list, idx, element):
     """
-    Replace an element in a list at a specific position without modifying the original list.
+    Replace an element in a list at a specific position without
+    modifying the original list.
 
     Args:
     - my_list: The input list.
