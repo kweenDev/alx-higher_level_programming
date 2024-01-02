18d676d95c26b6e6651188414c2c0516131256ad
kweenDev <refiloe15radebe@gmail.com>
2024-01-02 15:01:01 +0200

    Commit Message: Add solution for Task 8 Details: Implemented the Python function magic_calculation to match given bytecode. Ensured compliance with PEP8.

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,5 +3,11 @@
 
 def magic_calculation(a, b):
     if a < b:
-        return magic_calculation_102.add(a, b) + sum(range(4, 6))
-    return magic_calculation_102.sub(a, b)
+        add = __import__('magic_calculation_102').add
+        sub = __import__('magic_calculation_102').sub
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
