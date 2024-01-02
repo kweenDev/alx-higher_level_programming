20eb25e29fcd8da6c25b867fc0c941e40b390b54
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 00:53:24 +0200

    Code Python script 10-divisible_by_2.py for finding multiples of 2 in a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -19,5 +19,4 @@ if __name__ == "__main__":
     my_list = [0, 1, 2, 3, 4, 5, 6]
     list_result = divisible_by_2(my_list)
     for i in range(len(list_result)):
-        print("{:d} {:s} divisible by 2".format(my_list[i],
-                    "is" if list_result[i] else "is not"))
+        print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
