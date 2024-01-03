61873b8ed6863baa39036607ad6eabc67428ddbf
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 12:22:27 +0200

    Solve Task 15: Delete by value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 
-def delete_at(my_list=[], idx=0):
+def complex_delete(a_dictionary, value):
     """
     Deletes the item at a specific position in a list.
 
-    :param my_list: The input list.
-    :param idx: The index to delete.
-    :return: The modified list.
+    :param a_dictionary: The input.
+    :param value: The value to be deleted.
+    :return: The modified dictionary.
     """
-    if 0 <= idx < len(my_list):
-        del my_list[idx]
-    return my_list
+    if 0 <= value < len(a_dictionary):
+        del a_dictionary[value]
+    return a_dictionary
