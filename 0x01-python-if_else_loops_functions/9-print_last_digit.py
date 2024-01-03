dcd157d86a953def71cd7f420b83a78e15be3ecc
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 13:43:55 +0200

    Add updated 9-print_last_digit.py file

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,8 @@
 
 def print_last_digit(number):
     print(abs(number) % 10, end="")
-    return(abs(number) % 10)
+    return (abs(number) % 10)
+
 
 if __name__ == "__main__":
     print_last_digit(number)
