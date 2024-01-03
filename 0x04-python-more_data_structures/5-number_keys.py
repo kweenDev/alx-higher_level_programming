8a3dc46757a2095bb3d655960a346a1259513d13
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 11:33:52 +0200

    Solve Task 5: Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+
+    :param a_dictionary: The input dictionary.
+    :return: The number of keys.
+    """
+    return len(a_dictionary)
