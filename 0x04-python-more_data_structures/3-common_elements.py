3fdaddb2aaee36e7f77026543474da46809225e6
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 11:25:53 +0200

    Finish Task 3: Present in Both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements in two sets.
+
+    :param set_1: The first set.
+    :param set_2: The second set.
+    :return: A set of common elements.
+    """
+    return set_1.intersection(set_2)
