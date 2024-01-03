f651150c694ce730a24034129c9b040f9dedc815
kweenDev <refiloe15radebe@gmail.com>
2024-01-04 00:30:35 +0200

    Implement raise_exception_msg function in 6-raise_exception_msg.py

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Raise exception with message module
+"""
+
+
+def raise_exception_msg(message=""):
+    """
+    Raises a name exception with a message
+    Args:
+        message: The message to be raised with the exception
+    """
+    raise NameError(message)
