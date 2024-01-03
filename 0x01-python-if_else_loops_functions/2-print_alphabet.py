2a077af5a757192d30a3d6687d096e76ff5b5d65
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 13:22:23 +0200

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
-
-    """
-    Prints the ASCII alphabet in lowercase, excluding 'q' and
-    'e'.
-    """
-    for letter in range(97, 123):
-        print("{}".format(chr(letter)), end="")
+"""
+Prints the ASCII alphabet in lowercase, excluding 'q' and 'e'.
+"""
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
