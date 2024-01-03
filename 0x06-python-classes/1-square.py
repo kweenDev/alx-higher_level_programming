6af95881fa76fde6a34d85d947152b2b9d02b76f
kweenDev <refiloe15radebe@gmail.com>
2024-01-04 01:34:44 +0200

    Task 1: Define a Square class with a private instance attribute 'size' and instantiation with size.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a Square class"""
+
+
+class Square:
+    """Square class with a private size attribute"""
+
+    def __init__(self, size):
+        """Initializes a new instance of the Square class"""
+        self.__size = size
