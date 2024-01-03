b53523390b294999aa18dadb842975d8e13c9941
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 12:08:27 +0200

    Solve Task 13: Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def weighted_average(my_list=[]):
+    """
+    Computes the weighted average of a list of tuples.
+
+    :param my_list: List of tuples where each tuple contains
+    (value, weight).
+    :return: The weighted average.
+    """
+    if not my_list:
+        return 0
+
+    sum_values = sum(value * weight for value, weight in my_list)
+    sum_weights = sum(weight for _, weight in my_list)
+
+    return sum_values / sum_weights
