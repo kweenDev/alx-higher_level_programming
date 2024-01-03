8e15574707f92385cb8f4d024ae9e3f0b81b27c8
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 11:57:59 +0200

    Solve Task 12: Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    """
+    Converts a Roman numeral to an integer.
+
+    :param roman_string: The input Roman numeral string.
+    :return: The corresponding integer value.
+    """
+    roman_dict =
+    {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        value = roman_dict[char]
+        total += value if value >= prev_value else -value
+        prev_value = value
+
+    return total
