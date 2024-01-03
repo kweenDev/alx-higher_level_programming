f3ff6a01aab131f422239318022958775c95e8bf
kweenDev <refiloe15radebe@gmail.com>
2024-01-04 00:37:45 +0200

    Implement magic_calculation function in 102-magic_calculation.py

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+ByteCode to Python #4 module
+"""
+
+
+def magic_calculation(a, b):
+    """
+    Simulates the given Python bytecode
+    """
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            result += (a ** b) / i
+        except:
+            result = b + a
+            break
+    return result
