159e098f2cd3040f23fbae6e4c6e9002fc604a8e
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 12:15:11 +0200

    Solve Task 14: Squared by using map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
-
 def square_matrix_map(matrix=[]):
-    """
-    Computes the square value of all integers in a matrix using map.
-
-    :param matrix: A 2-dimensional array.
-    :return: A new matrix with each value being the square of the input.
-    """
     return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
