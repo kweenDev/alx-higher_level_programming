2e0620bdc5e37a12d29741408f84b408d8d02406
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 12:00:11 +0200

    Solve Task 11: Multiply by using map

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-
-
 def multiply_list_map(my_list=[], multiplier=0):
-    """
-    Multiplies all elements of a list by a specified multiplier using map.
-
-    :param my_list: The input list.
-    :param multiplier: The multiplier to apply.
-    :return: A new list with elements multiplied.
-    """
     return list(map(lambda x: x * multiplier, my_list))
