0ac10bf5a6c32af8dd6acc5c700a521e3ed64bb4
kweenDev <refiloe15radebe@gmail.com>
2024-01-03 11:40:36 +0200

    Solve Task 6: Print sorted dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    """
+    Prints a dictionary by ordered keys.
+
+    :param a_dictionary: The input dictionary.
+    """
+    for key in sorted(a_dictionary.keys()):
+        print(f"{key}: {a_dictionary[key]}")
