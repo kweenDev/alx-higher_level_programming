fb9a1743766bb07f1b0a4162b892c872fc479417
kweenDev <refiloe15radebe@gmail.com>
2024-01-04 16:04:10 +0200

    Task 2: Implement size validation for the Square class, ensuring 'size' is a positive integer.

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,29 +7,9 @@ class Square:
 
     def __init__(self, size=0):
         """Initializes a new instance of the Square class with optional size"""
-        self.__size = size
-
-    @property
-    def size(self):
-        """Getter method to retrieve the size"""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """Setter method to set the size with validation"""
-        if not isinstance(value, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = value
-
-
-# Additional method to display the dictionary representation
-def dict_(self):
-    """Returns a dictionary representation of the Square instance"""
-    return {'_Square__size': self.__size}
-
-
-# Attach the function to the class as a method
-Square.dict_ = dict_
+            self.__size = size
