5bd94d8353e199149b57bf8d965692d0e3a093be
kweenDev <refiloe15radebe@gmail.com>
2024-01-05 01:37:17 +0200

    Task 1: Implemented validation of integer values for width and height in Rectangle class.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,54 +1,42 @@
 #!/usr/bin/python3
-"""
-Module that defines a Rectangle class with width and height attributes.
-"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """
-    Defines a Rectangle class with width and height attributes.
-    """
+    """Represent a rectangle."""
+
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
-        Initializes a Rectangle instance with optional width and height.
-        """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """
-        Retrieves the width attribute.
-        """
+        """Get/set the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        Sets the width attribute with the given value.
-        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
-        """
-        Retrieves the height attribute.
-        """
+        """Get/set the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Sets the height attribute with the given value.
-        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
