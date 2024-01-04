c675c07eb6e486b4c38cbff3219fa6684948e7fb
kweenDev <refiloe15radebe@gmail.com>
2024-01-04 16:10:29 +0200

    Task 3: Add a public instance method 'area' to calculate the area of the Square.

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,16 +3,17 @@
 
 
 class Square:
-    """Square class with private size attribute, size validation, and area calculation"""
+    """Square class with private size attribute, size validation,
+    and area calculation"""
 
     def __init__(self, size=0):
-        """Initializes a new instance of the Square class with optional size"""
-        if not isinstance(value, int):
+        """Initialize a new instance of the Square class with optional size"""
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = value
+            self.__size = size
 
     def area(self):
         """Public instance method to calculate the area of the square"""
