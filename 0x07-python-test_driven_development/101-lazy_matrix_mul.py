acebf1da98bea2eb4851441024c83d261b329931
kweenDev <refiloe15radebe@gmail.com>
2024-01-08 05:55:11 +0200

    Add python files for test driven development

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module for lazy matrix multiplication using NumPy.
+"""
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Lazy matrix multiplication using NumPy.
+
+    Args:
+        m_a (list): First matrix.
+        m_b (list): Second matrix.
+
+    Returns:
+        np.ndarray: Resulting matrix.
+
+    Raises:
+        TypeError: If m_a or m_b is not a list of lists, or if elements are not numeric.
+        ValueError: If m_a or m_b is empty or if matrices cannot be multiplied.
+    """
+
+    return (np.matmul(m_a, m_b))
