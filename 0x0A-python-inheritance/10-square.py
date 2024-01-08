8253db52e6272befd43056d92b785ecc2c6564c7
kweenDev <refiloe15radebe@gmail.com>
2024-01-09 01:25:06 +0200

    Create a Square class that inherits from Rectangle with a constructor for size validation.

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,6 +4,34 @@
 """
 
 
+class Rectangle(BaseGeometry):
+    """
+    A class that inherits from BaseGeometry.
+    Instantiation with width and height.
+
+    Args:
+        width: Width of the rectangle.
+        height: Height of the rectangle.
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Calculates and returns the area of the rectangle.
+
+        Returns:
+            The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
 class Square(Rectangle):
     """
     A class that inherits from Rectangle.
