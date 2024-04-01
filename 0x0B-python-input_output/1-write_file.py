ec73443b2686ef2e08c1b916593890d17ea4022e
kweenDev <refiloe15radebe@gmail.com>
2024-04-01 18:11:10 +0200

    Include script for writing content to a text file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Module writes content to a text file and returns the number of characters."""
+"""
+Module writes content to a text file.
+"""
 
 
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF8) and returns the number of characters written.
+    Writes a string to a text file (UTF8).
     """
     with open(filename, 'w', encoding='utf-8') as file:
-        nb_charcters = file.write(text)
-        return nb_characters
+        return file.write(text)
