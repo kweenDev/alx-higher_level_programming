672f6eafce7d3c6cea36aec8003b1485a4cb5367
kweenDev <refiloe15radebe@gmail.com>
2024-04-01 19:37:24 +0200

    Implement script to convert class object to JSON representation

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -7,13 +7,4 @@ def class_to_json(obj):
     Returns the dictionary description with simple data structure for
     JSON serialization of an object.
     """
-    serializable_attributes = {}
-
-    for attr_name in dir(obj):
-        if not attr_name.startswith("__") \
-                and not callable(getattr(obj, attr_name)):
-            attr_value = getattr(obj, attr_name)
-            if isinstance(attr_value, (list, dict, str, int, bool)):
-                serializable_attributes[attr_name] = attr_value
-
-                return serializable_attributes
+    return obj.__dict__
