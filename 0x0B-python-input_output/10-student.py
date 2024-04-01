78b8e2a24457c97057b7e00ce8351c0ea3b704e2
kweenDev <refiloe15radebe@gmail.com>
2024-04-01 21:06:27 +0200

    Extend Student class with JSON serialization and desrialization methods

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -7,13 +7,13 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """
-        Initializes a Student instance with first_name, last_name, and age.
+        Initializes a new Student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self. attrs=None):
+    def to_json(self, attrs=None):
         """
         Retrieves a dictionary representation of a Student
         instance with optional attribute filtering.
