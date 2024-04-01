c012f6c6be613ce2efee49743ecea554a1137781
kweenDev <refiloe15radebe@gmail.com>
2024-04-01 18:14:42 +0200

    Implement script to append content to a text file and return character count

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module appends a string at the end of a text file."""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string at the end of a text file (UTF8) and
+    returns the number of characters added.
+    """
+    with open(filename, 'a', encoding='utf-8') as file:
+        nb_characters_added = file.write(text)
+        return nb_characters_added
