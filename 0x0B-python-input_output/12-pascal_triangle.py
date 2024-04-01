6ea81eea6508242300b1ad3461c7f9826f706a5a
kweenDev <refiloe15radebe@gmail.com>
2024-04-01 20:28:21 +0200

    Add script to generate Pascal's Triangle up to a given number of rows

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Generates Pascal's Triangle up to a given number of rows"""
+
+
+def pascal_triangle(n):
+    """
+    Generates Pascal's Triangle up to the nth row.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i -1][j])
+            row.append(1)
+            triangle.append(row)
+
+    return triangle
