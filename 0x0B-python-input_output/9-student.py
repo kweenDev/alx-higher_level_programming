8e3c4a6fc1c8cac0afd6a578862b71040d01bae8
kweenDev <refiloe15radebe@gmail.com>
2024-04-01 19:54:22 +0200

    Add Student class with JSON serialization methods

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,17 +6,15 @@ class Student:
     """Defines a student with first_name, last_name. and age attributes."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a Student instance with first_name, last_name, and age."""
+        """
+        Initializes a Student instance with first_name, last_name, and age.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        def to_json(self):
-            """
-            Retrieves a dictionary representation of a Student instance.
-            """
-            return {
-                    'first_name': self.first_name,
-                    'last_name': self.last_name,
-                    'age': self.age
-                    }
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        """
+        return self.__dict__
