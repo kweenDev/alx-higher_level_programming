3b5752a2bec41b2ea40b9f4d1a76d7fcb0a247d2
kweenDev <refiloe15radebe@gmail.com>
2024-04-05 09:11:30 +0200

    Update and correct syntax in files.

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- Convert database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+USE hbtn_0c_0;
 -- Convert table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- Display max temperature of each state ordered by state name
-SELECT state, MAX(temperature_fahrenheit) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
