d188c270318fc45f824887fdfc1828e8f013f7cd
kweenDev <refiloe15radebe@gmail.com>
2024-04-05 11:36:48 +0200

    Fix syntax error in 8-cities_of_california_subquery.sql file.

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,3 @@
 -- List all the cities of California from the hbtn_0d_usa database without using the JOIN keyword
 
-SELECT * FROM cities
-WHERE state_id = (
-    SELECT id FROM states WHERE name = 'California'
-)
-ORDER BY id ASC;
+SELECT id FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id;
