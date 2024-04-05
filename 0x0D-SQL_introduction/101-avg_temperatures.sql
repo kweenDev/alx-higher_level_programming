dcfc4a1f0d15ca6533924dddd1b587f42ca7675d
kweenDev <refiloe15radebe@gmail.com>
2024-04-05 08:57:41 +0200

    Update and correct syntax in files.

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,2 @@
 -- Display average temperature by city ordered by temperature (descending)
-SELECT city, AVG(temperature_fahrenheit) AS avg_temp
-FROM temperatures
-GROUP BY city
-ORDER BY avg_temp DESC;
+SELECT city, AVG(temperature_fahrenheit) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,2 @@
 -- Display max temperature of each state ordered by state name
-SELECT state, MAX(temperature_fahrenheit) AS max_temp
-FROM temperatures
-GROUP BY state
-ORDER BY state;
+SELECT state, MAX(temperature_fahrenheit) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
