3f33240a7d9bd10e277ec5e1567163414f73f648
kweenDev <refiloe15radebe@gmail.com>
2024-04-05 11:29:11 +0200

    Fix syntax error in 3-force_name.sql file.

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,8 +1,4 @@
 -- SQL script to create table force_name
 -- Create table force_name if not exists
 
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
