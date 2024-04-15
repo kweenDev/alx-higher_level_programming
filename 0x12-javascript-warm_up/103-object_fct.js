7a5e054adc6acd6b6f9e8bd4722249ae45bb5192
kweenDev <refiloe15radebe@gmail.com>
2024-04-15 16:15:17 +0200

    Update and fix errors.

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,21 +1,17 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
-  // Define the incr function inside the myObject object
-  incr: function () {
-    this.value++;
-  }
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
 };
 
-console.log(myObject);
-
-// Call the incr function to increment the value
 myObject.incr();
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
