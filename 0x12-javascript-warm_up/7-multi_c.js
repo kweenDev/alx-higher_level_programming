716c35a750174e85dd78a79eedb774c6fe7d6c0c
kweenDev <refiloe15radebe@gmail.com>
2024-04-15 16:23:26 +0200

    Update and fix errors.

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-
-const num = parseInt(arg);
-
-if (!isNaN(num) && num > 0) {
-  for (let i = 0; i < num; i++) {
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
