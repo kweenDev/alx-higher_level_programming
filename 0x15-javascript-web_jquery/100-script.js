e53b1c9f35e0f2932fa2d33ab04d8b229e4cf3ec
Refiloe Radebe <52778663+kweenDev@users.noreply.github.com>
2024-07-05 13:33:20 +0200

    Delete 0x15-javascript-web_jquery directory

--- 0x15-javascript-web_jquery/100-script.js
+++
@@ -1,5 +0,0 @@
-// Update the header text color to red when the document is loaded
-document.addEventListener('DOMContentLoaded', function () {
-  const header = document.querySelector('header');
-  header.style.color = '#FF0000';
-});
